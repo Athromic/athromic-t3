b9883a083169e4bacbb6aa5aa0f40728a09027fc
athromic <119989048+masonc-dev@users.noreply.github.com>
2024-07-12 06:56:14 -0700

    Redeploy for new env vars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default async function Home() {
               <h3 className="text-2xl font-bold">Documentation →</h3>
               <div className="text-lg">
                 Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
+                to deploy it!
               </div>
             </Link>
           </div>
