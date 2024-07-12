b474d715e108ec361067311c99bb1718c1b11ddd
athromic <119989048+masonc-dev@users.noreply.github.com>
2024-07-11 20:56:27 -0700

    Fixed some minor bugs causing builds to fail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { LatestPost } from "~/app/_components/post";
 import { getServerAuthSession } from "~/server/auth";
 import { api, HydrateClient } from "~/trpc/server";
-import { AlertCircle } from "lucide-react"
   
 
 
