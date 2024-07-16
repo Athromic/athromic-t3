693d570558e278d4c63ad0757236960affb964b4
athromic <119989048+masonc-dev@users.noreply.github.com>
2024-07-16 16:45:48 -0700

    :anger:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 import { withUt } from "uploadthing/tw";
-
-export default withUt({
+/** @type {import('tailwindcss').Config} */
+const config = {
   darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
  theme: {
     container: {
       center: true,
@@ -107,4 +110,6 @@ export default withUt({
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography"), require("@tailwindcss/forms"),],
-}) satisfies Config;
+} satisfies Config;
+
+export default config;
