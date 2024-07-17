cdb69cef81803f31aa4d57bcf8a45e338ecfb821
athromic <119989048+masonc-dev@users.noreply.github.com>
2024-07-17 10:45:11 -0700

    Remove grid background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,6 @@ import Image from "next/image";
 import { FAQ } from "@/components/home/faq";
 import  Card  from "@/components/home/card"
 
-export function GridBackgroundDemo() {
-  return (
-    <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
-      {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-    </div>
-  );
-}
-
 export default async function Home() {
   return (
     
