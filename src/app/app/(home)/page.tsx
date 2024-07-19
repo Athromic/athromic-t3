e4f4db274b1616043adb136c4a83ca6a7560e2b6
Mason <119989048+masonc-dev@users.noreply.github.com>
2024-07-18 19:45:47 -0700

    Update page.tsx

--- src/app/app/(home)/page.tsx
+++ src/app/app/(home)/page.tsx
@@ -2,7 +2,7 @@ import { SearchBar } from "@/components/app/home"
 
 export default async function SearchHome() {
   return (
-   <div>
+   <div className="z-10 w-full max-w-xl px-5 xl:px-0">
       <SearchBar />
    </div>
 )
