58fae155f22d449e4a7fac37224fafb5c830eb6f
athromic <119989048+masonc-dev@users.noreply.github.com>
2024-07-19 14:57:34 -0700

    Changed Bento

--- src/components/home/bento.tsx
+++ src/components/home/bento.tsx
@@ -294,6 +294,89 @@ const SkeletonFive = () => {
     </motion.div>
   );
 };
+
+const SkeletonSix = () => {
+  const first = {
+    initial: {
+      x: 20,
+      rotate: -5,
+    },
+    hover: {
+      x: 0,
+      rotate: 0,
+    },
+  };
+  const second = {
+    initial: {
+      x: -20,
+      rotate: 5,
+    },
+    hover: {
+      x: 0,
+      rotate: 0,
+    },
+  };
+  return (
+    <motion.div
+      initial="initial"
+      animate="animate"
+      whileHover="hover"
+      className="flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-row space-x-2"
+    >
+      <motion.div
+        variants={first}
+        className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
+      >
+        <Image
+          src="https://ucarecdn.com/de2ba8c0-f174-4129-b108-15a08eb08e5b/119989048%20(1).webp"
+          alt="avatar"
+          height="100"
+          width="100"
+          className="rounded-full h-10 w-10"
+        />
+        <p className="sm:text-sm text-xs text-center font-semibold text-neutral-500 mt-4">
+          Llama
+        </p>
+        <p className="border border-red-500 bg-red-100 dark:bg-red-900/20 text-red-600 text-xs rounded-full px-2 py-0.5 mt-4">
+          Delusional
+        </p>
+      </motion.div>
+      <motion.div className="h-full relative z-20 w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center">
+        <Image
+          src="https://ucarecdn.com/de2ba8c0-f174-4129-b108-15a08eb08e5b/119989048%20(1).webp"
+          alt="avatar"
+          height="100"
+          width="100"
+          className="rounded-full h-10 w-10"
+        />
+        <p className="sm:text-sm text-xs text-center font-semibold text-neutral-500 mt-4">
+          Claude
+        </p>
+        <p className="border border-green-500 bg-green-100 dark:bg-green-900/20 text-green-600 text-xs rounded-full px-2 py-0.5 mt-4">
+          Sensible
+        </p>
+      </motion.div>
+      <motion.div
+        variants={second}
+        className="h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center"
+      >
+        <Image
+          src="https://ucarecdn.com/de2ba8c0-f174-4129-b108-15a08eb08e5b/119989048%20(1).webp"
+          alt="avatar"
+          height="100"
+          width="100"
+          className="rounded-full h-10 w-10"
+        />
+        <p className="sm:text-sm text-xs text-center font-semibold text-neutral-500 mt-4">
+          ChatGPT
+        </p>
+        <p className="border border-orange-500 bg-orange-100 dark:bg-orange-900/20 text-orange-600 text-xs rounded-full px-2 py-0.5 mt-4">
+          Helpless
+        </p>
+      </motion.div>
+    </motion.div>
+  );
+};
 const items = [
   {
     title: "AI Content Generation",
@@ -350,5 +433,16 @@ const items = [
     header: <SkeletonFive />,
     className: "md:col-span-1",
     icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "Amazing Tailwindcss Grid Layout Examples",
+    description: (
+      <span className="text-sm">
+        Grids are cool, but Tailwindcss grids are cooler. In this article, we will learn how to create amazing Grid layouts with Tailwindcs grid and React.
+      </span>
+    ),
+    header: <SkeletonSix />,
+    className: "md:col-span-3",
+    icon: <IconTableColumn className="h-4 w-6 text-neutral-500" />,
   },
 ];
