d6cc682e8ffbaa8107b5498433291c1c1611b8ed
Mason <119989048+masonc-dev@users.noreply.github.com>
2024-07-18 19:44:26 -0700

    Update layout.tsx

---
+++ src/app/app/(home)/layout.tsx
@@ -0,0 +1,46 @@
+import "@/styles/globals.css";
+import cx from "classnames";
+import { sfPro, inter } from "./fonts";
+import Nav from "@/components/layout/nav";
+import Footer from "@/components/layout/footer";
+import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+export const metadata = {
+  title: "Athromic - App",
+  description:
+    "Coming Soon.",
+  metadataBase: new URL("https://www.athromic.com"),
+  icons: {
+    icon: '/favicon.ico', // /public path
+  },
+};
+
+import { TRPCReactProvider } from "@/trpc/react";
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={cx(sfPro.variable, inter.variable)}>
+        <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-100 via-cyan-50 to-pink-100">
+          <div className="relative fixed inset-0 -z-10">
+            <div className="absolute inset-0 bg-gradient-to-br from-transparent to-pink-100 opacity-50" />
+          </div>
+        </div>
+        <Nav />
+        <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
+          <TRPCReactProvider>
+            {children}
+          </TRPCReactProvider>
+        </main>
+        <Footer />
+        <VercelAnalytics />
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
