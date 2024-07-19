500e34607f6fa3323ba455d3de232f0c73cb774f
Mason <119989048+masonc-dev@users.noreply.github.com>
2024-07-18 23:03:23 -0700

    Update background-grid.tsx

--- src/components/shared/background-grid.tsx
+++ src/components/shared/background-grid.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export function GridBackground() {
   return (
-    <div className="h-[50rem] w-full  dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative flex items-center justify-center">
+    <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
       {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
     </div>
   );
 }
