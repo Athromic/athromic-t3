fa97f4e54a0d6d44652efcc17f9e978d909cace9
Mason <119989048+masonc-dev@users.noreply.github.com>
2024-07-23 09:57:31 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
-  experimental: {
-    // remove reactCompiler as it's not a valid key
-  },
   images: {
     domains: ["avatars.githubusercontent.com", "vercel.com", "images.unsplash.com", "assets.aceternity.com", "api.microlink.io","pbs.twimg.com","ucarecdn.com"],
   },
