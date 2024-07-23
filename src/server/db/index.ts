e835238ce0906ef471de565268e8b884a15e7fd9
athromic <119989048+masonc-dev@users.noreply.github.com>
2024-07-23 07:31:38 -0700

    Remove package-lock.yaml

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,16 +1,12 @@
-const { config } = require('dotenv');
-const { drizzle } = require('drizzle-orm/libsql');
-const { createClient } = require('@libsql/client');
+import { config } from 'dotenv';
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
 
 config({ path: '.env' });
 
-try {
-  const client = createClient({
-    url: process.env.TURSO_CONNECTION_URL,
-    authToken: process.env.TURSO_AUTH_TOKEN,
-  });
+const client = createClient({
+  url: process.env.TURSO_CONNECTION_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
+});
 
-  module.exports = { db: drizzle(client) };
-} catch (error) {
-  console.error('Error creating client or initializing database:', error);
-}
+export const db = drizzle(client);
