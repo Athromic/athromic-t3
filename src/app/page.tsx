81e32195b55434277df8664ab22fdbb7f7b94584
athromic <119989048+masonc-dev@users.noreply.github.com>
2024-07-26 22:57:45 -0700

    Change "backed by" text size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ const Home = () => {
             <SignupForm />
           </div>
           <section className="z-20 mt-10 flex items-center justify-center sm:mt-20 sm:mt-[39px]">
-            <p className="text-sm text-default-400">Backed by</p>&nbsp;
+            <p className="text-base text-default-400">Backed by</p>&nbsp;
             <p> mason</p>
           </section>
         </div>
