2de2587d8e5d5a39c7f6ea488abc2612abf16fd3
athromic <119989048+masonc-dev@users.noreply.github.com>
2024-07-26 22:51:04 -0700

    Change footer text size

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ import { Text } from "./text";
 
 export const Footer: React.FC = () => (
   <footer className="flex h-[80px] items-center justify-center text-center">
-    <Text className="inline-flex items-center gap-2">
+    <Text className="inline-flex items-center gap-2 text-base">
       Brought to you by{" "}
       <Anchor
         appearance="white"
