bac4c8d1990fab9c92a655620fe617431c9c1b65
athromic <119989048+masonc-dev@users.noreply.github.com>
2024-07-26 22:54:29 -0700

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,27 +1,8 @@
-import Image from "next/image";
 import * as React from "react";
-import { Anchor } from "./anchor";
 import { Text } from "./text";
 
 export const Footer: React.FC = () => (
   <footer className="flex h-[80px] items-center justify-center text-center">
-    <Text className="inline-flex items-center gap-2 text-base">
-      Brought to you by{" "}
-      <Anchor
-        appearance="white"
-        className="inline-flex items-center gap-2"
-        href="https://resend.com"
-        target="_blank"
-      >
-        <Image
-          alt=""
-          className="border-slate-7 inline-block rounded-full border"
-          height="20"
-          src="/static/resend.png"
-          width="20"
-        />
-        Resend
-      </Anchor>
-    </Text>
+    <Text className="inline-flex items-center gap-2 text-base">Athromic</Text>
   </footer>
 );
