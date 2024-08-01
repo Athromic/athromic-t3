25f0c10a1412bb6f0c30d99e7412a2227ae67453
Mason <119989048+masonc-dev@users.noreply.github.com>
2024-07-31 20:07:30 -0700

    Add status footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,6 +2,13 @@ import * as React from "react";
 
 export const Footer: React.FC = () => (
   <footer className="flex h-[80px] items-center justify-center text-center">
+<iframe
+  frameBorder="0"
+  height="30"
+  scrolling="no"
+  src="https://status.athromic.com/badge?theme=dark"
+  width="250"
+/>
     <p className="text-base inline-flex items-center gap-2 text-default-400 [text-shadow:_0_2px_2px_rgb(0_0_0_/_25%)] ">
       © 2024 Athromic. All rights reserved.
     </p>
